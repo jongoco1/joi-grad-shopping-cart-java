adc300e5a534d735c914f789da6552c0ece5b86b
Jillian Ongoco <jsongoco.ucsd@gmail.com>
2022-04-11 13:29:57 -0500

    started to implement BOGO

--- src/main/java/com/thoughtworks/codepairing/model/ShoppingCart.java
+++ src/main/java/com/thoughtworks/codepairing/model/ShoppingCart.java
@@ -3,6 +3,8 @@ package com.thoughtworks.codepairing.model;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.Map;
+import java.util.HashMap;
 
 public class ShoppingCart {
     private List<Product> products;
@@ -20,6 +22,8 @@ public class ShoppingCart {
         products.add(product);
     }
 
+    // function: removeProduct
+    // purpose: remove an item from the shopping cart
     public List<Product> removeProduct(String name) {
         int index = 0;
         List<Product> revisedList = new ArrayList<Product>(products);
@@ -37,6 +41,31 @@ public class ShoppingCart {
         }
         products = revisedList;
         return revisedList;
+    }
+
+    // function: X for V sales (X=number of an item, V=value of one set of an item)
+    // purpose: helper function to allow for a different type of sale (i.e. BOGO, etc.)
+    public Order setSales(int numItemsInSet, int numFreeItems) {
+        Map<String, Integer> counter = new HashMap<>();
+        for (Product product : products) {
+            // parse thru shopping cart
+            // if item does not exist in hashmap, create new key w product name & value=1
+            // else add 1 to the current value held by key
+            String prodName = product.getName();
+            if (counter.containsKey(prodName)) {
+                counter.put(prodName, counter.get(prodName) + 1);
+            }
+            else {
+                counter.put(prodName, 1);
+            }
+        }
+        // calculate price based on numItemsInSet and numFreeItems
+        // use division and mod operators
+        // case: cannot be combined with any other offers
+        // case: combined with other offers (much more difficult)
+
+        // placeholder
+        return null;
     }
 
     public Order checkout() {
