9c412fa3167c062c9091036894b7dbfbefcf6899
Jillian Ongoco <ocognoj@gmail.com>
2022-04-11 10:45:16 -0500

    Wrote "removeProduct" function
    Passed my test for "removeProduct"

--- src/main/java/com/thoughtworks/codepairing/model/ShoppingCart.java
+++ src/main/java/com/thoughtworks/codepairing/model/ShoppingCart.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.codepairing.model;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -12,14 +13,38 @@ public class ShoppingCart {
         this.products = products;
     }
 
+    public List<Product> getProducts() { return products; }
+    public Customer getCustomer() { return customer; }
+
     public void addProduct(Product product) {
         products.add(product);
+    }
+
+    public List<Product> removeProduct(String name) {
+        int index = 0;
+        List<Product> revisedList = new ArrayList<Product>(products);
+        for (Product product : products) {
+            // case: item does NOT exist in shopping cart
+            if (index >= products.size()) {
+                System.out.println("Item " + name + " no found in shopping cart");
+            }
+            // case: item exists in shopping cart
+            if (product.getName().equals(name)) {
+                revisedList.remove(index);
+                System.out.println("Item " + name + " removed shopping cart");
+            }
+            else { index++; }
+        }
+        products = revisedList;
+        return revisedList;
     }
 
     public Order checkout() {
         double totalPrice = 0;
 
         int loyaltyPointsEarned = 0;
+        System.out.println("In ShoppingCart.checkout(),\n"
+            + "products array size: " + products.size());
         for (Product product : products) {
             double discount = 0;
             if (product.getProductCode().startsWith("DIS_10")) {
--- src/test/java/com/thoughtworks/codepairing/model/ShoppingCartTest.java
+++ src/test/java/com/thoughtworks/codepairing/model/ShoppingCartTest.java
@@ -22,55 +22,91 @@ public class ShoppingCartTest {
 
     @Test
     public void shouldCalculatePriceWithNoDiscount() {
+        System.out.println("test: calculate PRICE w/ NO discount");
         List<Product> products = asList(new Product(PRICE, "", PRODUCT));
         ShoppingCart cart = new ShoppingCart(customer, products);
         Order order = cart.checkout();
 
         assertEquals(100.0, order.getTotalPrice(), 0.0);
+        System.out.println("test end: calculate PRICE w/ NO discount");
     }
 
     @Test
     public void shouldCalculateLoyaltyPointsWithNoDiscount() {
+        System.out.println("test: calculate POINTS w/ NO discount");
         List<Product> products = asList(new Product(PRICE, "", PRODUCT));
         ShoppingCart cart = new ShoppingCart(customer, products);
         Order order = cart.checkout();
 
         assertEquals(20, order.getLoyaltyPoints());
+        System.out.println("test end: calculate POINTS w/ NO discount");
     }
 
     @Test
     public void shouldCalculatePriceFor10PercentDiscount() {
+        System.out.println("test: calculate PRICE w/ 10% discount");
         List<Product> products = asList(new Product(PRICE, "DIS_10_ABCD", PRODUCT));
         ShoppingCart cart = new ShoppingCart(customer, products);
         Order order = cart.checkout();
 
         assertEquals(90.0, order.getTotalPrice(), 0.0);
+        System.out.println("test end: calculate PRICE w/ 10% discount");
     }
 
     @Test
     public void shouldCalculateLoyaltyPointsFor10PercentDiscount() {
+        System.out.println("test: calculate POINTS w/ 10% discount");
         List<Product> products = asList(new Product(PRICE, "DIS_10_ABCD", PRODUCT));
         ShoppingCart cart = new ShoppingCart(customer, products);
         Order order = cart.checkout();
 
         assertEquals(10, order.getLoyaltyPoints());
+        System.out.println("test end: calculate POINTS w/ 10% discount");
     }
 
     @Test
     public void shouldCalculatePriceFor15PercentDiscount() {
+        System.out.println("test: calculate PRICE w/ 15% discount");
         List<Product> products = asList(new Product(PRICE, "DIS_15_ABCD", PRODUCT));
         ShoppingCart cart = new ShoppingCart(customer, products);
         Order order = cart.checkout();
 
         assertEquals(85.0, order.getTotalPrice(), 0.0);
+        System.out.println("test end: calculate PRICE w/ 15% discount");
     }
 
     @Test
     public void shouldCalculateLoyaltyPointsFor15PercentDiscount() {
+        System.out.println("test: calculate POINTS w/ 15% discount");
         List<Product> products = asList(new Product(PRICE, "DIS_15_ABCD", PRODUCT));
         ShoppingCart cart = new ShoppingCart(customer, products);
         Order order = cart.checkout();
 
         assertEquals(6, order.getLoyaltyPoints());
+        System.out.println("test end: calculate POINTS w/ 15% discount");
     }
+
+    @Test
+    public void shouldRemoveExistingItemFromList() {
+        System.out.println("test: remove existing item from list");
+        List<Product> products = asList(new Product(PRICE, "DIS_15_ABCD", PRODUCT));
+        ShoppingCart cart = new ShoppingCart(customer, products);
+        cart.removeProduct(PRODUCT);
+        Order order = cart.checkout();
+
+        assertEquals(0, order.getLoyaltyPoints());
+        System.out.println("test end: remove existing item from list");
+    }
+
+//    @Test
+//    public void tryRemovingNonExistingItemFromList() {
+//        System.out.println("test: try removing non-existing item from list");
+//        List<Product> products = asList(new Product(PRICE, "DIS_15_ABCD", PRODUCT));
+//        ShoppingCart cart = new ShoppingCart(customer, products);
+//        cart.removeProduct("item");
+//        Order order = cart.checkout();
+//
+//        assertEquals(0, order.getLoyaltyPoints());
+//        System.out.println("test end: try removing non-existing item from list");
+//    }
 }
